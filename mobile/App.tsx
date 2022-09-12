344b8fb04116a12ae4711c8d6e2d4ebccfe9df5e
Rubendvb <rubenvasquez556@gmail.com>
2022-09-12 04:25:26 -0300

    Criando versão mobile com expo

---
+++ mobile/App.tsx
@@ -0,0 +1,51 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+
+interface ButtonProps {
+  title: string;
+}
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello World! React Native</Text>
+
+      <Button title="Send 1" />
+      <Button title="Send 2" />
+      <Button title="Send 3" />
+      <Button title="Hello world!" />
+      <StatusBar style="auto" backgroundColor="#FFFF" />
+    </View>
+  );
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <TouchableOpacity>
+      <Text style={styles.button}>{props.title}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  title: {
+    color: "#fff",
+    fontSize: 24,
+  },
+
+  button: {
+    color: "#fff",
+    fontSize: 24,
+    backgroundColor: "blue",
+    marginBottom: 10,
+    padding: 10,
+    borderRadius: 10,
+  },
+});
