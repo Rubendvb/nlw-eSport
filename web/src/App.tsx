b20b1c3b6f16a0164f0c7a4b0f02a7523bac0d30
Rubendvb <rubenvasquez556@gmail.com>
2022-09-18 17:15:46 -0300

    Finalizando versão desktop

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import * as Dialog from "@radix-ui/react-dialog";
 import { GameBanner } from "./components/GameBanner";
 import { CreateAdBanner } from "./components/CreateAdBanner";
+import axios from "axios";
 
 import "./assets/styles/main.css";
 import logoImg from "./assets/Logo.svg";
@@ -21,11 +22,9 @@ function App() {
   const [games, setGames] = useState<Game[]>([]);
 
   useEffect(() => {
-    fetch("http://localhost:3333/games")
-      .then((response) => response.json())
-      .then((data) => {
-        setGames(data);
-      });
+    axios("http://localhost:3333/games").then((response) => {
+      setGames(response.data);
+    });
   }, []);
 
   return (
